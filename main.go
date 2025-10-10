e889762158e41123b52885e1ff98746cf4053e5c
Sazk47 <jonathan212006hasard@gmail.com>
2025-10-10 09:58:45 +0200

    [ADD] adding directory, style.css, fixing main.go and index.html

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
-	})
+var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
-	fs := http.FileServer(http.Dir("static/"))
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func playHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Erreur lors de l'analyse du formulaire", http.StatusBadRequest)
+		return
+	}
+	column := r.FormValue("column")
+
+	log.Println("Colonne jouée :", column)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/play", playHandler)
+
+	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.ListenAndServe(":8080", nil)
+	log.Println("Serveur démarré sur http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
